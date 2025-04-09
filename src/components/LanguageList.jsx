3bfcbea9bc1b068eae1f643df93289289f8133ae
Carlo Costa <carlo.costa92@gmail.com>
2025-04-09 08:59:44 +0200

    feat: changed font-style

--- src/components/LanguageList.jsx
+++ src/components/LanguageList.jsx
@@ -13,30 +13,32 @@ export default function LanguagesList() {
 
 
     return (
-        <div className="main-container">
-            <div>
-                {languages.map((language) => (
-                    <Language
-                        key={language.id}
-                        title={language.title}
-                        onClick={() => languageClick(language.id)}
-                    />
-                ))}
+        <main>
+            <div className="main-container">
+                <div>
+                    {languages.map((language) => (
+                        <Language
+                            key={language.id}
+                            title={language.title}
+                            onClick={() => languageClick(language.id)}
+                        />
+                    ))}
 
+                </div>
+
+                {selectedLanguage ? (
+                    <div className="card">
+                        <h2>{selectedLanguage.title}</h2>
+                        <p>{selectedLanguage.description}</p>
+                    </div>
+
+                ) : (
+                    <div className="card">
+                        <p>Nessun linguaggio selezionato</p>
+                    </div>
+                )
+                }
             </div>
-
-            {selectedLanguage ? (
-                <div className="card">
-                    <h2>{selectedLanguage.title}</h2>
-                    <p>{selectedLanguage.description}</p>
-                </div>
-
-            ) : (
-                <div className="card">
-                    <p>Nessun linguaggio selezionato</p>
-                </div>
-            )
-            }
-        </div>
+        </main>
     );
 }
